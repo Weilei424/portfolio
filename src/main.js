8042273b1133c0ce884427cdb37076a664ab08ae
Zhenxu Wang <mason123wp@gmail.com>
2023-05-23 21:42:12 -0400

    implements blur and unblur functions for project images

---
+++ src/main.js
@@ -0,0 +1,19 @@
+function unblurImage1() {
+    var image = document.getElementById('card-img-top1');
+    image.style.filter = 'blur(0)';
+}
+
+function blurImage1() {
+    var image = document.getElementById('card-img-top1');
+    image.style.filter = 'blur(5px)';
+}
+
+function unblurImage2() {
+    var image = document.getElementById('card-img-top2');
+    image.style.filter = 'blur(0)';
+}
+
+function blurImage2() {
+    var image = document.getElementById('card-img-top2');
+    image.style.filter = 'blur(5px)';
+}
