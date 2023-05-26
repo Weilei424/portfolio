01713c7232c3275ccac81eea2cea16b6e8608eed
Zhenxu Wang <mason123wp@gmail.com>
2023-05-26 17:42:43 -0400

    add one more row (4) project cards

--- src/main.js
+++ src/main.js
@@ -27,3 +27,53 @@ function blurImage3() {
     var image = document.getElementById('card-img-top3');
     image.style.filter = 'blur(5px)';
 }
+
+function unblurImage4() {
+    var image = document.getElementById('card-img-top4');
+    image.style.filter = 'blur(0)';
+}
+
+function blurImage4() {
+    var image = document.getElementById('card-img-top4');
+    image.style.filter = 'blur(5px)';
+}
+
+function unblurImage5() {
+    var image = document.getElementById('card-img-top5');
+    image.style.filter = 'blur(0)';
+}
+
+function blurImage5() {
+    var image = document.getElementById('card-img-top5');
+    image.style.filter = 'blur(5px)';
+}
+
+function unblurImage6() {
+    var image = document.getElementById('card-img-top6');
+    image.style.filter = 'blur(0)';
+}
+
+function blurImage6() {
+    var image = document.getElementById('card-img-top6');
+    image.style.filter = 'blur(5px)';
+}
+
+function unblurImage7() {
+    var image = document.getElementById('card-img-top7');
+    image.style.filter = 'blur(0)';
+}
+
+function blurImage7() {
+    var image = document.getElementById('card-img-top7');
+    image.style.filter = 'blur(5px)';
+}
+
+function unblurImage8() {
+    var image = document.getElementById('card-img-top8');
+    image.style.filter = 'blur(0)';
+}
+
+function blurImage8() {
+    var image = document.getElementById('card-img-top8');
+    image.style.filter = 'blur(5px)';
+}
