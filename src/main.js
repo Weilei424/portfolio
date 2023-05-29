f1128fb00cf94339deda186ac55d30fddc1e7236
Zhenxu Wang <mason123wp@gmail.com>
2023-05-28 23:32:03 -0400

    add github repo link for project#1

--- src/main.js
+++ src/main.js
@@ -1,79 +1,83 @@
 function unblurImage1() {
     var image = document.getElementById('card-img-top1');
-    image.style.filter = 'blur(0)';
+    var link = document.getElementById('card-link1');
+    image.style.filter = 'blur(5px)';
+    link.style.display = 'block';
 }
 
 function blurImage1() {
     var image = document.getElementById('card-img-top1');
-    image.style.filter = 'blur(5px)';
+    var link = document.getElementById('card-link1');
+    image.style.filter = 'blur(0)';
+    link.style.display = 'none';
 }
 
 function unblurImage2() {
     var image = document.getElementById('card-img-top2');
-    image.style.filter = 'blur(0)';
+    image.style.filter = 'blur(5px)';
 }
 
 function blurImage2() {
     var image = document.getElementById('card-img-top2');
-    image.style.filter = 'blur(5px)';
+    image.style.filter = 'blur(0)';
 }
 
 function unblurImage3() {
     var image = document.getElementById('card-img-top3');
-    image.style.filter = 'blur(0)';
+    image.style.filter = 'blur(5px)';
 }
 
 function blurImage3() {
     var image = document.getElementById('card-img-top3');
-    image.style.filter = 'blur(5px)';
+    image.style.filter = 'blur(0)';
 }
 
 function unblurImage4() {
     var image = document.getElementById('card-img-top4');
-    image.style.filter = 'blur(0)';
+    image.style.filter = 'blur(5px)';
 }
 
 function blurImage4() {
     var image = document.getElementById('card-img-top4');
-    image.style.filter = 'blur(5px)';
+    image.style.filter = 'blur(0)';
 }
 
 function unblurImage5() {
     var image = document.getElementById('card-img-top5');
-    image.style.filter = 'blur(0)';
+    image.style.filter = 'blur(5px)';
 }
 
 function blurImage5() {
     var image = document.getElementById('card-img-top5');
-    image.style.filter = 'blur(5px)';
+    image.style.filter = 'blur(0)';
 }
 
 function unblurImage6() {
     var image = document.getElementById('card-img-top6');
-    image.style.filter = 'blur(0)';
+    image.style.filter = 'blur(5px)';
 }
 
 function blurImage6() {
     var image = document.getElementById('card-img-top6');
-    image.style.filter = 'blur(5px)';
+    image.style.filter = 'blur(0)';
 }
 
 function unblurImage7() {
     var image = document.getElementById('card-img-top7');
-    image.style.filter = 'blur(0)';
+    image.style.filter = 'blur(5px)';
 }
 
 function blurImage7() {
     var image = document.getElementById('card-img-top7');
-    image.style.filter = 'blur(5px)';
+    image.style.filter = 'blur(0)';
 }
 
 function unblurImage8() {
     var image = document.getElementById('card-img-top8');
-    image.style.filter = 'blur(0)';
+    image.style.filter = 'blur(5px)';
 }
 
 function blurImage8() {
     var image = document.getElementById('card-img-top8');
-    image.style.filter = 'blur(5px)';
+    image.style.filter = 'blur(0)';
 }
