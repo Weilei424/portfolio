0078babc45f2f9a97539d480780ed18ff933702b
Zhenxu Wang <mason123wp@gmail.com>
2023-05-31 12:32:33 -0400

    add function calls

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ function unblurImage1() {
     var text = document.getElementById('card-text1');
     image.style.filter = 'blur(5px)';
     text.style.display = 'block';
+    text.style.fontWeight = 'bold';
 }
 
 function blurImage1() {
@@ -21,6 +22,7 @@ function unblurImage2() {
     var text = document.getElementById('card-text2');
     image.style.filter = 'blur(5px)';
     text.style.display = 'block';
+    text.style.fontWeight = 'bold';
 }
 
 function blurImage2() {
@@ -32,60 +34,114 @@ function blurImage2() {
 
 function unblurImage3() {
     var image = document.getElementById('card-img-top3');
+    var text = document.getElementById('card-text3');
     image.style.filter = 'blur(5px)';
+    text.style.display = 'block';
+    text.style.fontWeight = 'bold';
 }
 
 function blurImage3() {
     var image = document.getElementById('card-img-top3');
+    var text = document.getElementById('card-text3');
     image.style.filter = 'blur(0)';
+    text.style.display = 'none';
+}
+
+function jumpTo3() {
+    window.open("https://github.com/Weilei424/Wallet-Watcher", '_blank');
 }
 
 function unblurImage4() {
     var image = document.getElementById('card-img-top4');
+    var text = document.getElementById('card-text4');
     image.style.filter = 'blur(5px)';
+    text.style.display = 'block';
+    text.style.fontWeight = 'bold';
 }
 
 function blurImage4() {
     var image = document.getElementById('card-img-top4');
+    var text = document.getElementById('card-text4');
     image.style.filter = 'blur(0)';
+    text.style.display = 'none';
+}
+
+function jumpTo4() {
+    window.open("https://github.com/Weilei424/Wallet-Watcher", '_blank');
 }
 
 function unblurImage5() {
     var image = document.getElementById('card-img-top5');
+    var text = document.getElementById('card-text5');
     image.style.filter = 'blur(5px)';
+    text.style.display = 'block';
+    text.style.fontWeight = 'bold';
 }
 
 function blurImage5() {
     var image = document.getElementById('card-img-top5');
+    var text = document.getElementById('card-text5');
     image.style.filter = 'blur(0)';
+    text.style.display = 'none';
+}
+
+function jumpTo5() {
+    window.open("https://github.com/Weilei424/Wallet-Watcher", '_blank');
 }
 
 function unblurImage6() {
     var image = document.getElementById('card-img-top6');
+    var text = document.getElementById('card-text6');
     image.style.filter = 'blur(5px)';
+    text.style.display = 'block';
+    text.style.fontWeight = 'bold';
 }
 
 function blurImage6() {
     var image = document.getElementById('card-img-top6');
+    var text = document.getElementById('card-text6');
     image.style.filter = 'blur(0)';
+    text.style.display = 'none';
+}
+
+function jumpTo6() {
+    window.open("https://github.com/Weilei424/Wallet-Watcher", '_blank');
 }
 
 function unblurImage7() {
     var image = document.getElementById('card-img-top7');
+    var text = document.getElementById('card-text7');
     image.style.filter = 'blur(5px)';
+    text.style.display = 'block';
+    text.style.fontWeight = 'bold';
 }
 
 function blurImage7() {
     var image = document.getElementById('card-img-top7');
+    var text = document.getElementById('card-text7');
     image.style.filter = 'blur(0)';
+    text.style.display = 'none';
+}
+
+function jumpTo7() {
+    window.open("https://github.com/Weilei424/Wallet-Watcher", '_blank');
 }
 
 function unblurImage8() {
     var image = document.getElementById('card-img-top8');
+    var text = document.getElementById('card-text8');
     image.style.filter = 'blur(5px)';
+    text.style.display = 'block';
+    text.style.fontWeight = 'bold';
 }
 
 function blurImage8() {
     var image = document.getElementById('card-img-top8');
+    var text = document.getElementById('card-text8');
     image.style.filter = 'blur(0)';
+    text.style.display = 'none';
+}
+
+function jumpTo8() {
+    window.open("https://github.com/Weilei424/Wallet-Watcher", '_blank');
 }
